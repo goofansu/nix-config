6878260903f59e338a19ed8561fdb8cf2377630e
Yejun Su <yejun@hey.com>
2023-02-19 23:03:57 +0800

    add git config in home.nix

--- home.nix
+++ home.nix
@@ -14,7 +14,28 @@
 
   programs.git = {
     enable = true;
-    includes = [{ path = "~/.config/nixpkgs/gitconfig"; }];
+    userName = "Yejun Su";
+    userEmail = "yejun@hey.com";
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+    };
+    signing = {
+      key = null;
+      signByDefault = true;
+      gpgPath = "/usr/local/bin/gpg";
+    };
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      pull = { rebase = true; };
+      github = { user = "goofansu"; };
+    };
+    ignores = [
+      ".DS_Store"
+      "*.log*"
+      ".elixir_ls"
+      "node_modules"
+    ];
     delta = {
       enable = true;
     };
