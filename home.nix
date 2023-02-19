1cb3d18e482a5182f3b52c67b5877a0803807b4c
Yejun Su <yejun@hey.com>
2023-02-19 21:42:22 +0800

    add programs.git

--- home.nix
+++ home.nix
@@ -3,4 +3,14 @@
   home.homeDirectory = "/Users/james";
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
+
+  home.packages = [];
+
+  # As a rule of thumb, if it’s supported by home-manager,
+  # install the program using programs.<program>.
+  # If not, add it to home.packages.
+  programs.git = {
+    enable = true;
+    includes = [{ path = "~/.config/nixpkgs/gitconfig"; }];
+  };
 }
