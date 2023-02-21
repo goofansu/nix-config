179bd2bb6903cabf60b0918048e589d1e2b03723
Yejun Su <yejun@hey.com>
2023-02-21 13:32:50 +0800

    suppress bash deprecate warning

--- home.nix
+++ home.nix
@@ -42,6 +42,7 @@
   };
 
   home.file.".asdfrc".text = "legacy_version_file = yes";
+  home.file.".bash_profile".text = "export BASH_SILENCE_DEPRECATION_WARNING=1";
   home.file.".gemrc".text = "gem: --no-document";
   home.file.".xrayconfig".text = ":editor: '/opt/homebrew/bin/emacsclient -s gui -c $file'";
 
