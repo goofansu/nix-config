35955e14bbe989283e81cab47bebd7061ad35300
Yejun Su <yejun@hey.com>
2023-02-22 10:29:55 +0800

    add comments

--- packages.nix
+++ packages.nix
@@ -2,31 +2,43 @@
 
 {
   home.packages = with pkgs; [
-    ansible
-    asciinema
-    asdf-vm
+    # GNU softwares
     coreutils
-    deno
-    exercism
-    fd
-    ffmpeg_5
-    flyctl
-    gist
+    findutils
     gnugrep
     gnused
-    htop
+    wget
+
+    # Nix
+    nix-prefetch-github
+
+    # Doom Emacs prerequisites
+    deno       # format JSON
+    fd         # better GNU find
+    pandoc     # markdown compiler
+    ripgrep    # search tool
+    shellcheck # shell script linting
+    zstd       # undo list compression
+
+    # media processing
+    ffmpeg_5
     imagemagick
+
+    # utils
+    asciinema
+    exercism
+    gist
+    htop
     inetutils
     jq
     lnav
-    nix-prefetch-github
-    pandoc
-    ripgrep
-    shellcheck
     tealdeer
     tokei
     tree
-    wget
-    zstd
+
+    # dev
+    ansible
+    asdf-vm
+    flyctl
   ];
 }
