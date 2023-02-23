bd02936acfd568521193e93f8c4b9a007fdc2b00
Yejun Su <yejun@hey.com>
2023-02-23 20:48:50 +0800

    support dry-run
    home-manager switch --dry-run

--- emacs.nix
+++ emacs.nix
@@ -9,8 +9,12 @@
   home.activation = {
     installDoomEmacs = lib.hm.dag.entryAfter ["writeBoundary"] ''
       if [ ! -d $HOME/.emacs.d ]; then
-        ${pkgs.git}/bin/git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs $HOME/.emacs.d
-        ${pkgs.git}/bin/git clone https://github.com/goofansu/.doom.d $HOME/.doom.d
+        if [ -v DRY_RUN ]; then
+          echo "Running installDoomEmacs"
+        else
+          ${pkgs.git}/bin/git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs $HOME/.emacs.d
+          ${pkgs.git}/bin/git clone https://github.com/goofansu/.doom.d $HOME/.doom.d
+        fi
       fi
     '';
   };
