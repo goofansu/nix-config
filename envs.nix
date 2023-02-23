2c9000a9df5cfbafc3150864112837abc3a6d0bc
Yejun Su <yejun@hey.com>
2023-02-23 14:50:40 +0800

    move env to envs.nix

---
+++ envs.nix
@@ -0,0 +1,24 @@
+{
+  home.sessionPath = [
+    "$HOME/.emacs.d/bin"
+  ];
+
+  home.sessionVariables = {
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+
+    # editor
+    EDITOR = "emacsclient -s term -t";
+    VISUAL = "emacsclient -s term -t";
+
+    # Emacs server name
+    EMACS_SERVER_NAME = "gui";
+
+    # Erlang
+    KERL_BUILD_DOCS = "yes";
+    ERL_AFLAGS = "-kernel shell_history enabled";
+
+    # quiet direnv
+    DIRENV_LOG_FORMAT = "";
+  };
+}
--- home.nix
+++ home.nix
@@ -3,16 +3,12 @@
   home.homeDirectory = "/Users/james";
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
-
-  home.sessionVariables = {
-    DIRENV_LOG_FORMAT = "";
-  };
-
   fonts.fontconfig.enable = true;
 
   imports = [
     ./packages.nix
     ./files.nix
+    ./envs.nix
     ./emacs.nix
     ./fish.nix
     ./git.nix
