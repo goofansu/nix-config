ba0ecb8bd8b74050b3b3d1c0d42157b7b32c174d
Yejun Su <yejun@hey.com>
2023-02-24 16:07:18 +0800

    add Brewfile

--- files.nix
+++ files.nix
@@ -2,4 +2,6 @@
   home.file.".asdfrc".text = "legacy_version_file = yes";
   home.file.".gemrc".text = "gem: --no-document";
   home.file.".xrayconfig".text = ":editor: 'emacsclient -s gui -c $file'";
+  home.file.".Brewfile".source = ./Brewfile;
+  home.file.".Brewfile.lock.json".source = ./Brewfile.lock.json;
 }
