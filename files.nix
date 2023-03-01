ad8e9ca62a7a179da2667be06b16ef2697753b6a
Yejun Su <yejun@hey.com>
2023-03-01 15:09:27 +0800

    use homebrew emacs for xrayconfig

--- files.nix
+++ files.nix
@@ -1,7 +1,8 @@
 {
   home.file.".asdfrc".text = "legacy_version_file = yes";
   home.file.".gemrc".text = "gem: --no-document";
-  home.file.".xrayconfig".text = ":editor: 'emacsclient -s gui -c $file'";
+  home.file.".xrayconfig".text =
+    ":editor: '/opt/homebrew/bin/emacsclient -s gui -c $file'";
   home.file.".Brewfile".source = ./Brewfile;
   home.file.".Brewfile.lock.json".source = ./Brewfile.lock.json;
 }
