2ac699432e3d94ba537b31b78e7d9726567864d6
Yejun Su <yejun@hey.com>
2023-04-11 11:01:40 +0800

    Revert "add agenda_files"
    This reverts commit dfa5ced343d07160610514553f0fea91034b86e7.

--- files.nix
+++ files.nix
@@ -1,9 +1,8 @@
 {
-  home.file.".agenda_files".source = ./orgmode/agenda_files;
   home.file.".asdfrc".text = "legacy_version_file = yes";
   home.file.".gemrc".text = "gem: --no-document";
   home.file.".xrayconfig".text =
     ":editor: '/opt/homebrew/bin/emacsclient -s gui -c $file'";
-  home.file.".Brewfile".source = ./homebrew/Brewfile;
-  home.file.".Brewfile.lock.json".source = ./homebrew/Brewfile.lock.json;
+  home.file.".Brewfile".source = ./Brewfile;
+  home.file.".Brewfile.lock.json".source = ./Brewfile.lock.json;
 }
