f2c8a8fec414413c01a8d316c8d4bc60fae0d705
Yejun Su <yejun@hey.com>
2023-05-10 16:38:17 +0800

    [fish] remove period from description

--- fish.nix
+++ fish.nix
@@ -54,31 +54,31 @@
     ];
     functions = {
       cat = {
-        description = "A cat(1) clone with wings.";
+        description = "A cat(1) clone with wings";
         body = "if command -sq bat; bat $argv; end";
       };
       find = {
-        description = "A simple, fast and user-friendly alternative to `find`.";
+        description = "A simple, fast and user-friendly alternative to `find`";
         body = "if command -sq fd; fd $argv; end";
       };
       gist = {
-        description = "Work with GitHub gists.";
+        description = "Work with GitHub gists";
         body = "if command -sq gh; gh gist $argv; end";
       };
       rm = {
-        description = "Ask before removing a file.";
+        description = "Ask before removing a file";
         body = "command rm -i $argv";
       };
 
       # GnuPG
       gpg-encrypt = {
-        description = "Encrypt a file with GnuPG.";
+        description = "Encrypt a file with GnuPG";
         body = "gpg --encrypt --sign --recipient yejun@hey.com $argv[1]";
       };
 
       # macOS
       reset-launchpad = {
-        description = "Reset macOS Launchpad.";
+        description = "Reset macOS Launchpad";
         body = ''
           defaults write com.apple.dock ResetLaunchPad -bool true
           killall Dock
@@ -87,35 +87,35 @@
 
       # Emacs commands
       e = {
-        description = "Edit file in Emacs.";
+        description = "Edit file in Emacs";
         body = "emacsclient -s term -a '' -nw $argv";
       };
       ediff = {
-        description = "Diff files in Emacs.";
+        description = "Diff files in Emacs";
         body = ''
           emacsclient -s term -nw -u -e "(ediff \"$argv[1]\" \"$argv[2]\")"
         '';
       };
       egit = {
-        description = "Git in Emacs.";
+        description = "Git in Emacs";
         body = ''
           emacsclient -s term -nw -u -e '(magit-status)'
         '';
       };
       eman = {
-        description = "Man page in Emacs.";
+        description = "Man page in Emacs";
         body = ''
           emacsclient -s term -nw -u -e "(man \"$argv[1]\")"
         '';
       };
       ekill = {
-        description = "Kill Emacs.";
+        description = "Kill Emacs";
         body = "emacsclient -s term -e '(kill-emacs)'";
       };
 
       # Fuzzy find everything!
       fssh = {
-        description = "Fuzzy find and ssh into a host.";
+        description = "Fuzzy find and ssh into a host";
         body = ''
           rg --ignore-case '^host [^*]' ~/.ssh/* | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
         '';
@@ -123,33 +123,33 @@
 
       # Git
       gcl = {
-        description = "Fuzzy find and list commits of the selected git branch.";
+        description = "Fuzzy find and list commits of the selected git branch";
         body = ''
           git br | fzf | awk '{print $1}' | read -l result; and git log --oneline $result
         '';
       };
       gcb = {
-        description = "Fuzzy find and checkout the selected git branch.";
+        description = "Fuzzy find and checkout the selected git branch";
         body = ''
           git br | fzf | awk '{print $1}' | read -l result; and git co $result
         '';
       };
       gco = {
-        description = "Fuzzy find and checkout the selected pull request.";
+        description = "Fuzzy find and checkout the selected pull request";
         body = ''
           gh pr list | fzf | awk '{print $1}' | read -l result; and gh co $result
         '';
       };
       gcd = {
-        description = "Checkout the develop branch.";
+        description = "Checkout the develop branch";
         body = "git co develop";
       };
       gcm = {
-        description = "Checkout the main branch.";
+        description = "Checkout the main branch";
         body = "git br -l master main | sed 's/^* //' | xargs git co";
       };
       gp = {
-        description = "Git pull current branch.";
+        description = "Git pull current branch";
         body = ''
           git pull origin $(git rev-parse --abbrev-ref HEAD)
         '';
@@ -173,13 +173,13 @@
 
       # Elixir dev
       hex-package = {
-        description = "Fetch Elixir package config.";
+        description = "Fetch Elixir package config";
         body = "mix hex.info $argv | grep 'Config:' | sed 's/Config: //g'";
       };
 
       # OpenApply dev
       release-pr-id = {
-        description = "Fetch the id of release pull request.";
+        description = "Fetch the id of release pull request";
         body = ''
           gh pr list -s open | grep 'release/' | awk '{print $1}'
         '';
