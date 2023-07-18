c7f7731e2f10f98a3cfe7aba7f96b5c49fb35ac0
Yejun Su <yejun@hey.com>
2023-07-19 01:07:06 +0800

    installEmacsConfig

--- emacs.nix
+++ emacs.nix
@@ -2,13 +2,12 @@
 
 {
   home.activation = {
-    installDoomEmacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    installEmacsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       if [ ! -d $HOME/.emacs.d ]; then
         if [ -v DRY_RUN ]; then
-          echo "Running installDoomEmacs"
+          echo "Running installEmacsConfig"
         else
-          ${pkgs.git}/bin/git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs $HOME/.emacs.d
-          ${pkgs.git}/bin/git clone https://github.com/goofansu/.doom.d $HOME/.doom.d
+          ${pkgs.git}/bin/git clone https://github.com/goofansu/rune $HOME/.emacs.d
         fi
       fi
     '';
