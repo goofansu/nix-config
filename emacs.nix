84d55031ccdc88e54eddf205559d3590bd9b508b
Yejun Su <yejun@hey.com>
2023-07-23 01:53:25 +0800

    install Doom Emacs to new location

--- emacs.nix
+++ emacs.nix
@@ -3,11 +3,12 @@
 {
   home.activation = {
     installEmacsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ ! -d $HOME/.emacs.d ]; then
+      if [ ! -d $HOME/.config/emacs ]; then
         if [ -v DRY_RUN ]; then
           echo "Running installEmacsConfig"
         else
-          ${pkgs.git}/bin/git clone https://github.com/goofansu/rune $HOME/.emacs.d
+          ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+          ${pkgs.git}/bin/git clone https://github.com/goofansu/.doom.d $HOME/.config/doom
         fi
       fi
     '';
