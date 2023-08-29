cfeaa1a6cb4e3dfbb3372b39d52b508901d274ab
Yejun Su <yejun@hey.com>
2023-08-30 00:06:18 +0800

    install pass-otp extension with password-store

--- rest.nix
+++ rest.nix
@@ -81,6 +81,7 @@
 
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
     settings = {
       PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
     };
