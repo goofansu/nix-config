199df0ad4e53d30c2ba4002ec96e83d56df63d68
Yejun Su <yejun@hey.com>
2023-09-04 12:13:31 +0800

    use emacsclient from nix store for xrayconfig

--- files.nix
+++ files.nix
@@ -1,5 +1,7 @@
+{ pkgs, ... }:
+
 {
   home.file.".gemrc".text = "gem: --no-document";
   home.file.".xrayconfig".text =
-    ":editor: '/opt/homebrew/bin/emacsclient -s gui -nc $file'";
+    ":editor: '/etc/profiles/per-user/james/bin/emacsclient -s gui -nc $file'";
 }
