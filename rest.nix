5e21ed1a29962043fa779ad1579d4fbf64a7eff6
Yejun Su <yejun@hey.com>
2023-09-07 15:06:47 +0800

    enable atuin auto_sync

--- rest.nix
+++ rest.nix
@@ -16,6 +16,12 @@
   programs.atuin = {
     enable = true;
     flags = [ "--disable-up-arrow" ];
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "prefix";
+    };
   };
 
   programs.bat = {
