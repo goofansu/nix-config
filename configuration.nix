4bd6f1a3727671a5e1a207dace52a36f762254df
Yejun Su <goofan.su@gmail.com>
2023-09-16 23:25:32 +0800

    refactor configuration.nix
    remove nix.config.allowUnfree option

--- configuration.nix
+++ configuration.nix
@@ -1,55 +1,61 @@
 {
-  # the stateVersion of nix-darwin
-  system.stateVersion = 4;
-
-  # Make sure the nix daemon always runs
+  # Auto upgrade nix package and the daemon service
   services.nix-daemon.enable = true;
 
-  # Creates a fish configuration sourcing needed environment changes
+  # Create fish configs that loads the nix-darwin environment
   programs.fish.enable = true;
 
-  # Enable the flakes feature
-  nix.settings.experimental-features = "nix-command flakes";
+  # Used for backwards compatibility of nix-darwin
+  system.stateVersion = 4;
 
-  # Add myself to trusted-users
-  nix.settings.trusted-users = [ "root" "james" ];
+  # Nix
+  nix = {
+    settings = {
+      # Enable the Nix flakes feature
+      experimental-features = "nix-command flakes";
 
-  # Add my flakes templates to nix registry
-  nix.registry.nix-templates = {
-    from = {
-      id = "goofansu";
-      ref = "templates";
-      type = "indirect";
+      # Add myself to trusted-users to work with direnv
+      trusted-users = [ "root" "james" ];
     };
-    to = {
-      type = "git";
-      url = "https://git.sr.ht/~goofansu/nix-templates";
+
+    # Add flake registries
+    registry = {
+      nix-templates = {
+        from = {
+          id = "goofansu";
+          ref = "templates";
+          type = "indirect";
+        };
+        to = {
+          type = "git";
+          url = "https://git.sr.ht/~goofansu/nix-templates";
+        };
+      };
     };
   };
 
-  # Allow installing unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # Homebrew
-  homebrew.enable = true;
-  homebrew.onActivation.cleanup = "uninstall";
-  homebrew.casks = [
-    "1password"
-    "anki"
-    "arc"
-    "cleanshot"
-    "dash"
-    "deepl"
-    "google-chrome"
-    "hey"
-    "mysqlworkbench"
-    "postman"
-    "prince"
-    "raycast"
-    "rapidapi"
-    "slack"
-    "viscosity"
-    "zoom"
-    "zotero"
-  ];
+  homebrew = {
+    enable = true;
+    onActivation = { cleanup = "uninstall"; };
+    casks = [
+      "1password"
+      "anki"
+      "arc"
+      "cleanshot"
+      "dash"
+      "deepl"
+      "google-chrome"
+      "hey"
+      "mysqlworkbench"
+      "postman"
+      "prince"
+      "raycast"
+      "rapidapi"
+      "slack"
+      "viscosity"
+      "zoom"
+      "zotero"
+    ];
+  };
 }
