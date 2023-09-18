529b58094137dc6e3771edbdc4c42b411d251d21
Yejun Su <goofan.su@gmail.com>
2023-09-18 21:46:43 +0800

    move EMACS_SERVER_NAME to Emacs config
    When running Emacs GUI, set EMACS_SERVER_NAME to gui.

--- envs.nix
+++ envs.nix
@@ -9,9 +9,6 @@
     EDITOR = "emacsclient -s term -t";
     VISUAL = "emacsclient -s term -t";
 
-    # Emacs server name
-    EMACS_SERVER_NAME = "gui";
-
     # Erlang
     KERL_BUILD_DOCS = "yes";
     ERL_AFLAGS = "-kernel shell_history enabled";
