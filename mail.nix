8fcc43d042bef9dc3e7123966acc052699300a5a
Yejun Su <goofan.su@gmail.com>
2024-01-13 23:08:21 +0800

    add work account

--- mail.nix
+++ mail.nix
@@ -1,36 +1,43 @@
 { config, pkgs, ... }:
 
 let
-  name = "Yejun Su";
-  email = "goofan.su@gmail.com";
+  gmailSettings = {
+    mbsync = {
+      enable = true;
+      create = "both";
+      expunge = "both";
+      patterns = [ "*" "[Gmail]*" ];
+    };
+    imap = {
+      host = "imap.gmail.com";
+      port = 993;
+    };
+    msmtp.enable = true;
+    smtp = {
+      host = "smtp.gmail.com";
+      port = 587;
+      tls.useStartTls = true;
+    };
+  };
 in {
   accounts.email = {
     maildirBasePath = ".mail";
     accounts = {
-      gmail = {
+      personal = {
         primary = true;
-        address = "${email}";
-        userName = "${email}";
-        realName = "${name}";
-        passwordCommand = "${pkgs.pass}/bin/pass ${email}";
-        maildir = { path = "."; };
-        mbsync = {
-          enable = true;
-          create = "both";
-          expunge = "both";
-          patterns = [ "*" "[Gmail]*" ];
-        };
-        imap = {
-          host = "imap.gmail.com";
-          port = 993;
-        };
-        msmtp.enable = true;
-        smtp = {
-          host = "smtp.gmail.com";
-          port = 587;
-          tls.useStartTls = true;
-        };
-      };
+        address = "goofan.su@gmail.com";
+        userName = "goofan.su@gmail.com";
+        realName = "Yejun Su";
+        passwordCommand = "${pkgs.pass}/bin/pass goofan.su@gmail.com";
+        maildir = { path = "Personal"; };
+      } // gmailSettings;
+      work = {
+        address = "james.su@managebac.com";
+        userName = "james.su@managebac.com";
+        realName = "James Su";
+        passwordCommand = "${pkgs.pass}/bin/pass james.su@managebac.com";
+        maildir = { path = "Work"; };
+      } // gmailSettings;
     };
   };
 
