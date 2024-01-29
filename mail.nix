67f6102bf415f54893bfd74ec44b735cda688fc4
Yejun Su <goofan.su@gmail.com>
2024-01-29 21:40:11 +0800

    remove work account from accounts.email

--- mail.nix
+++ mail.nix
@@ -1,25 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  gmailSettings = {
-    mbsync = {
-      enable = true;
-      create = "both";
-      expunge = "both";
-      patterns = [ "*" "[Gmail]*" ];
-    };
-    imap = {
-      host = "imap.gmail.com";
-      port = 993;
-    };
-    msmtp.enable = true;
-    smtp = {
-      host = "smtp.gmail.com";
-      port = 587;
-      tls.useStartTls = true;
-    };
-  };
-in {
+{
   accounts.email = {
     maildirBasePath = ".mail";
     accounts = {
@@ -30,14 +11,23 @@ in {
         realName = "Yejun Su";
         passwordCommand = "${pkgs.pass}/bin/pass goofan.su@gmail.com";
         maildir = { path = "Personal"; };
-      } // gmailSettings;
-      work = {
-        address = "james.su@managebac.com";
-        userName = "james.su@managebac.com";
-        realName = "James Su";
-        passwordCommand = "${pkgs.pass}/bin/pass james.su@managebac.com";
-        maildir = { path = "Work"; };
-      } // gmailSettings;
+        mbsync = {
+          enable = true;
+          create = "both";
+          expunge = "both";
+          patterns = [ "*" "[Gmail]*" ];
+        };
+        imap = {
+          host = "imap.gmail.com";
+          port = 993;
+        };
+        msmtp.enable = true;
+        smtp = {
+          host = "smtp.gmail.com";
+          port = 587;
+          tls.useStartTls = true;
+        };
+      };
     };
   };
 
