5d42d7579f6358902b16ce400d07b51fcf078e64
Yejun Su <goofan.su@gmail.com>
2024-06-13 08:16:26 +0800

    add back pass-otp

--- gpg.nix
+++ gpg.nix
@@ -1,8 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.gpg = {
     enable = true;
+    package = pkgs.gnupg;
     publicKeys = [
       {
         source = ./gpg/pubkey.asc;
@@ -22,6 +23,7 @@
 
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
     settings = {
       PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
       PASSWORD_STORE_KEY = "3C2DE0F1FB93D0EE";
