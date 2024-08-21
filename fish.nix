e55056f827d994a047005b9b2ef56a048b36ae91
Yejun Su <goofan.su@gmail.com>
2024-08-21 18:22:41 +0800

    remove unused fish plugins

--- fish.nix
+++ fish.nix
@@ -6,35 +6,10 @@
     shellInit = ''
       set -U hydro_multiline true;
     '';
-    plugins = [
-      {
-        name = "done";
-        src = pkgs.fetchFromGitHub {
-          owner = "franciscolourenco";
-          repo = "done";
-          rev = "d6abb267bb3fb7e987a9352bc43dcdb67bac9f06";
-          sha256 = "6oeyN9ngXWvps1c5QAUjlyPDQwRWAoxBiVTNmZ4sG8E=";
-        };
-      }
-      {
-        name = "hydro";
-        src = pkgs.fetchFromGitHub {
-          owner = "jorgebucaran";
-          repo = "hydro";
-          rev = "41b46a05c84a15fe391b9d43ecb71c7a243b5703";
-          sha256 = "zmEa/GJ9jtjzeyJUWVNSz/wYrU2FtqhcHdgxzi6ANHg=";
-        };
-      }
-      {
-        name = "dracula";
-        src = pkgs.fetchFromGitHub {
-          owner = "dracula";
-          repo = "fish";
-          rev = "0e51af5e5346e5d24efabd43fb4631e2a8fd1b70";
-          sha256 = "YXh6pPJ9dJDPpq1kX5xd1edoOaH6jDq8pDOkx3k03/0=";
-        };
-      }
-    ];
+    plugins = [{
+      name = "hydro";
+      src = pkgs.fishPlugins.hydro.src;
+    }];
     functions = {
       cat = {
         description = "A cat(1) clone with wings";
