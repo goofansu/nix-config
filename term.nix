0ee6eaf4f83832125fabdd5282546704922305c2
Yejun Su <goofan.su@gmail.com>
2024-08-21 22:28:05 +0800

    simplify kitty's configuration

--- term.nix
+++ term.nix
@@ -3,43 +3,38 @@
 {
   programs.kitty = {
     enable = true;
-    theme = "Dracula";
+    theme = "Modus Vivendi";
     font = {
       name = "Iosevka Comfy Fixed";
       size = 18;
     };
-    darwinLaunchOptions = [ "--listen-on=unix:/tmp/kitty.sock" ];
     shellIntegration = {
       mode = "no-cursor";
       enableFishIntegration = true;
     };
     settings = {
-      shell = "${pkgs.fish}/bin/fish";
-      allow_remote_control = "yes";
-      confirm_os_window_close = 1;
-      copy_on_select = "yes";
+      # Cursor
       cursor_blink_interval = 0;
-      cursor_shape = "block";
-      enabled_layouts = "splits, stack";
-      macos_option_as_alt = "left";
+
+      # Scrollback
+      scrollback_pager_history_size = 40;
+
+      # Mouse
+      copy_on_select = "clipboard";
+
+      # Window
+      confirm_os_window_close = 1;
+
+      # Advanced
+      shell = "${pkgs.fish}/bin/fish --login --interactive";
+
+      # OS specific tweaks
+      macos_option_as_alt = "both";
       macos_traditional_fullscreen = "yes";
-      scrollback_pager_history_size = 20;
-      tab_bar_style = "powerline";
-      window_padding_width = 4;
     };
-    extraConfig = ''
-      action_alias launch_window launch --type window --cwd current
-      action_alias launch_overlay launch --type overlay --cwd current
-    '';
     keybindings = {
-      "cmd+]" = "next_window";
-      "cmd+0x1e" = "next_window";
-      "cmd+[" = "previous_window";
-      "cmd+0x21" = "previous_window";
-      "cmd+d" = "launch_window --location vsplit";
-      "shift+cmd+d" = "launch_window --location hsplit";
-      "shift+cmd+t" = "detach_window new-tab";
-      "shift+cmd+Enter" = "toggle_layout stack";
+      "cmd+enter" = "launch --cwd=current";
+      "shift+cmd+enter" = "toggle_layout stack";
     };
   };
 }
