036111b63449f725abc9a9afa6a4fc3b54b08dbe
Yejun Su <goofan.su@gmail.com>
2024-09-04 08:59:23 +0800

    configure hunspell dictionaries for jinx

--- files.nix
+++ files.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
 
 {
-  home.file.".gemrc".text = "gem: --no-document";
-  home.file.".xrayconfig".text =
-    ":editor: '/etc/profiles/per-user/james/bin/emacsclient -s gui -nc $file'";
-  home.file.".config/enchant/hunspell/".source =
-    "${pkgs.hunspellDicts.en_US}/share/hunspell/";
+  home.file = {
+    ".gemrc".text = "gem: --no-document";
+    ".xrayconfig".text =
+      ":editor: '/etc/profiles/per-user/james/bin/emacsclient -s gui -nc $file'";
+
+    # Dictionaries for jinx
+    ".config/enchant/hunspell/en_US.aff".source =
+      "${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.aff";
+    ".config/enchant/hunspell/en_US.dic".source =
+      "${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.dic";
+    ".config/enchant/hunspell/en_GB.aff".source =
+      "${pkgs.hunspellDicts.en_GB-ise}/share/hunspell/en_GB.aff";
+    ".config/enchant/hunspell/en_GB.dic".source =
+      "${pkgs.hunspellDicts.en_GB-ise}/share/hunspell/en_GB.dic";
+  };
 }
