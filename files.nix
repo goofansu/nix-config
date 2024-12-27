d94f3c82bc72e848cc1613ebb8a1d473345b8182
Yejun Su <goofan.su@gmail.com>
2024-12-27 17:58:58 +0800

    formatting

--- files.nix
+++ files.nix
@@ -13,9 +13,13 @@
     '';
 
     # Dictionaries for jinx
-    ".config/enchant/hunspell/en_US.aff".source = "${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.aff";
-    ".config/enchant/hunspell/en_US.dic".source = "${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.dic";
-    ".config/enchant/hunspell/en_GB.aff".source = "${pkgs.hunspellDicts.en_GB-ise}/share/hunspell/en_GB.aff";
-    ".config/enchant/hunspell/en_GB.dic".source = "${pkgs.hunspellDicts.en_GB-ise}/share/hunspell/en_GB.dic";
+    ".config/enchant/hunspell/en_US.aff".source =
+      "${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.aff";
+    ".config/enchant/hunspell/en_US.dic".source =
+      "${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.dic";
+    ".config/enchant/hunspell/en_GB.aff".source =
+      "${pkgs.hunspellDicts.en_GB-ise}/share/hunspell/en_GB.aff";
+    ".config/enchant/hunspell/en_GB.dic".source =
+      "${pkgs.hunspellDicts.en_GB-ise}/share/hunspell/en_GB.dic";
   };
 }
