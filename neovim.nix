44685085a286aba19c3803d84a4846448618ed87
Yejun Su <goofan.su@gmail.com>
2024-12-29 11:51:55 +0800

    simplify neovim config

--- neovim.nix
+++ neovim.nix
@@ -3,58 +3,28 @@
 {
   programs.neovim = {
     enable = true;
+
     viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
     plugins = with pkgs.vimPlugins; [
-      lazy-nvim
       telescope-nvim
-      plenary-nvim
       telescope-fzf-native-nvim
     ];
     extraLuaConfig = ''
       vim.g.mapleader = " "
       vim.g.maplocalleader = " "
-      require("lazy").setup(
-        {
-          {
-            'nvim-telescope/telescope.nvim',
-            dependencies = {
-              'nvim-lua/plenary.nvim',
-              'nvim-telescope/telescope-fzf-native.nvim'
-            },
-            config = function()
-              local telescope = require('telescope')
-              local builtin = require('telescope.builtin')
 
-              telescope.setup({
-                defaults = {
-                  mappings = {
-                    i = {
-                      ["<C-j>"] = "move_selection_next",
-                      ["<C-k>"] = "move_selection_previous",
-                    },
-                  },
-                  file_ignore_patterns = {
-                    "node_modules",
-                    ".git"
-                  }
-                }
-              })
+      local telescope = require('telescope')
+      telescope.setup()
+      telescope.load_extension('fzf')
 
-              telescope.load_extension('fzf')
-
-              -- Keymaps
-              vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-              vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
-              vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
-            end
-          },
-        },
-        {
-          rocks = { enabled = false },
-          install = { missing = false },
-          checker = { enabled = false }
-        }
-      )
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     '';
   };
 }
